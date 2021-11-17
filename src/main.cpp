fbad17af6ad069a091c0826845543fcaee7fcc84
mengshangqi <mengshangqi@qq.com>
2021-11-17 15:14:35 +0800

    add TypePromation

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,13 @@
 
 #include "common/error_codes.h"
 #include "common/type_cast.h"
+#include "common/type_promation.h"
 
 using namespace common;
 using namespace ErrorCode;
+
+class T : TypePromation<T> {};
+
 int main() {
   std::cout << ErrorCodeStr<0> << std::endl;
   std::cout << OK << std::endl;
