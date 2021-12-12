2c50ff8dcc938850480695c24637bf40cd2a58b7
plutolove <meng-shangqi@qq.com>
2021-12-12 18:23:31 +0800

    add jit

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 
+#include "common/cow.h"
 #include "common/error_codes.h"
 #include "common/type_cast.h"
 #include "common/type_promation.h"
+#include "jit/sql_jit.h"
 
 using namespace common;
 using namespace ErrorCode;
 
 class T : TypePromation<T> {};
+class TT : COW<T> {};
 
 int main() {
+  sql::SQLJit jit;
   std::cout << ErrorCodeStr<0> << std::endl;
   std::cout << OK << std::endl;
   return 0;
