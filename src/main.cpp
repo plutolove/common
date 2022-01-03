8605dad6df7744dd93d5485a253aa290e4c9f03a
plutolove <meng-shangqi@qq.com>
2022-01-03 22:11:01 +0800

    支持clang编译声喊曾module

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,45 @@
 #include <iostream>
 
+#include "clang/CodeGen/CodeGenAction.h"
+#include "clang/Driver/Action.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/TextDiagnosticPrinter.h"
 #include "common/cow.h"
 #include "common/error_codes.h"
 #include "common/type_cast.h"
 #include "common/type_promation.h"
 #include "jit/sql_jit.h"
-
 using namespace common;
 using namespace ErrorCode;
 
-class T : TypePromation<T> {};
-class TT : COW<T> {};
+llvm::Module* compile(const char* filename) {
+  clang::CompilerInstance compiler;
+  auto invocation = std::make_shared<clang::CompilerInvocation>();
+  llvm::IntrusiveRefCntPtr<clang::DiagnosticIDs> DiagID(
+      new clang::DiagnosticIDs());
+  auto diagOptions = new clang::DiagnosticOptions();
+  clang::DiagnosticsEngine Diags(
+      DiagID, diagOptions,
+      new clang::TextDiagnosticPrinter(llvm::errs(), diagOptions));
+  std::vector<const char*> arguments = {filename};
+  clang::CompilerInvocation::CreateFromArgs(*invocation, arguments, Diags);
+
+  compiler.setInvocation(invocation);
+  compiler.setDiagnostics(new clang::DiagnosticsEngine(
+      DiagID, diagOptions,
+      new clang::TextDiagnosticPrinter(llvm::errs(), diagOptions)));
+  std::unique_ptr<clang::CodeGenAction> action(new clang::EmitLLVMOnlyAction());
+  compiler.ExecuteAction(*action);
+  std::unique_ptr<llvm::Module> result = action->takeModule();
+  llvm::errs() << *result;
+  return result.release();
+}
+
 
 int main() {
-  sql::SQLJit jit;
-  std::cout << ErrorCodeStr<0> << std::endl;
-  std::cout << OK << std::endl;
+  // sql::SQLJit jit;
+  // std::cout << ErrorCodeStr<0> << std::endl;
+  // std::cout << OK << std::endl;
+  compile("/home/meng/CLionProjects/common/src/test.cpp");
   return 0;
 }
---
+++ src/test.cpp
@@ -0,0 +1,1 @@
+int mmmax() { return 0; }
