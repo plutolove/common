a4b67b2837fbbd33429a47064cd8846527035d77
plutolove <meng-shangqi@qq.com>
2022-06-04 13:19:02 +0800

    [params] add fmt

--- src/main.cpp
+++ src/main.cpp
@@ -2,41 +2,16 @@
 #include <llvm/IR/Module.h>
 
 #include <iostream>
+#include <set>
 
-#include "clang/CodeGen/CodeGenAction.h"
-#include "clang/Driver/Action.h"
-#include "clang/Frontend/CompilerInstance.h"
-#include "clang/Frontend/TextDiagnosticPrinter.h"
 #include "common/error_codes.h"
 #include "common/type_cast.h"
 #include "common/type_promation.h"
+#include "fmt/format.h"
 #include "jit/sql_jit.h"
 
 using namespace common;
 using namespace ErrorCode;
-
-llvm::Module* compile(const char* filename) {
-  clang::CompilerInstance compiler;
-  auto invocation = std::make_shared<clang::CompilerInvocation>();
-  llvm::IntrusiveRefCntPtr<clang::DiagnosticIDs> DiagID(
-      new clang::DiagnosticIDs());
-  auto diagOptions = new clang::DiagnosticOptions();
-  clang::DiagnosticsEngine Diags(
-      DiagID, diagOptions,
-      new clang::TextDiagnosticPrinter(llvm::errs(), diagOptions));
-  std::vector<const char*> arguments = {filename};
-  clang::CompilerInvocation::CreateFromArgs(*invocation, arguments, Diags);
-
-  compiler.setInvocation(invocation);
-  compiler.setDiagnostics(new clang::DiagnosticsEngine(
-      DiagID, diagOptions,
-      new clang::TextDiagnosticPrinter(llvm::errs(), diagOptions)));
-  std::unique_ptr<clang::CodeGenAction> action(new clang::EmitLLVMOnlyAction());
-  compiler.ExecuteAction(*action);
-  std::unique_ptr<llvm::Module> result = action->takeModule();
-  llvm::errs() << *result;
-  return result.release();
-}
 
 int main() {
   sql::SQLJit::CompiledModule res =
@@ -72,8 +47,8 @@ int main() {
     std::cout << "name: " << kv.first << "\t addr: " << kv.second << std::endl;
     auto* func = reinterpret_cast<int (*)()>(kv.second);
     // std::string val = "x";
-    std::cout << "res: " << func() << std::endl;
-    // std::cout << val << std::endl;
+    std::cout << fmt::format("res: {}\n", func());
+    // std::cout << val << std::endl;f
   }
   std::set<int> s;
   auto* func = reinterpret_cast<void (*)(std::set<int>*)>(
